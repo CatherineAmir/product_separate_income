879ea09b8fa35274b7369d0e512c25d41e6f7eeb
CatherineAmir <catherine.amir20@gmail.com>
2024-08-11 16:22:51 +0300

    cruise_name and guide

--- models/account_move.py
+++ models/account_move.py
@@ -5,6 +5,8 @@ class AccountMoveCruise(models.Model):
     _inherit = "account.move"
 
     cruise_id = fields.Many2one("cruise.cruise", string="Cruise")
+    cruise_name=fields.Selection(
+        [("Dahabia", 'dahabia'), ("Sun", 'sun'), ("Moon", 'moon'), ("Star", "star"), ("Nile", 'nile')])
 
 
 
@@ -13,6 +15,7 @@ class AccountMoveCruise(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move.line'
     sight_seeing = fields.Boolean(string="is sight seeing?")
+    sight_seeing_price_person = fields.Float(string="Sight Seeing for Person in EGP", )
     persons = fields.Float()
     nights = fields.Integer()
     main_line = fields.Boolean(default=False, copy=False)
@@ -23,13 +26,15 @@ class AccountMove(models.Model):
     default_total = fields.Monetary(copy=False)
     children = fields.Selection([('0', '0'), ('1', '1'), ('2', '2')], default='0', string="Children")
     cabinet_number = fields.Integer(default=1)
+    guest_nationality = fields.Many2one('res.country', string="Guest Nationality", tracking=True, )
+    cruise_name = fields.Selection(related='move_id.cruise_name', string="cruise name",store=True)
 
-
-    @api.constrains('product_id', 'account_id', 'price_unit', 'tax_ids')
+    @api.constrains('product_id',
+                    'account_id', 'price_unit', 'tax_ids')
     def split_account_lines(self):
         for r in self:
 
-            if r.move_id.state != 'draft' or r.move_id.move_type == 'entry':
+            if r.move_id.state != 'draft' or r.move_id.move_type not in ['out_invoice','out_refund']:
                 return
             # print("r.account_id.id",r.account_id.id)
             # print("r.product_id.product_tmpl_id.property_account_income_id.id",r.product_id.product_tmpl_id.property_account_income_id.id)
@@ -50,7 +55,7 @@ class AccountMove(models.Model):
                     if account_line.separation_criteria == 'percentage':
 
                         total_taken += r.price_unit * account_line.percentage_amount
-
+                        qty = r.persons * r.nights
                         vals = {
                             'account_id': account_line.income_account_id.id,
 
@@ -59,19 +64,32 @@ class AccountMove(models.Model):
                             'name': account_line.label,
                             'move_id': r.move_id.id,
                             'price_unit': (r.price_unit * account_line.percentage_amount),
-                            'quantity': account_line.quantity,
+                            'quantity': qty,
 
                         }
 
+                        if account_line.is_base:
+                            r.with_context(check_move_validity=False).write(vals)
+                        else:
+                            r.move_id.with_context(check_move_validity=False)
+                            new_line = r.copy(vals)
+                            new_line.with_context(check_move_validity=False).write(vals)
+                            r.with_context(check_move_validity=True)
+
 
                     else:
+                        amount_line=account_line.fixed_amount
                         if not r.sight_seeing and account_line.is_sight_seeing:
                             continue
                         if account_line.is_sight_seeing:
                             sight_seeing=True
                             qty = r.persons
+                            if r.sight_seeing_price_person:
+                                amount_line=r.sight_seeing_price_person
+
+
                             print("qty sight seeing", qty)
-                            if r.move_id.currency_id.id == r.move_id.company_id.currency_id.id:
+                            if r.guest_nationality.id==65 or not r.guest_nationality :
                                 if account_line.is_egyptian:
                                     pass
                                 else:
@@ -96,46 +114,44 @@ class AccountMove(models.Model):
                             'account_id': account_line.income_account_id.id,
                             'name': account_line.label,
                             'move_id': r.move_id.id,
-                            'price_unit': account_line.fixed_amount * rate,
+                            'price_unit': amount_line * rate,
                             'quantity': qty,
                             'sight_seeing':sight_seeing,
                         }
                         # print("account_line.fixed_amount*rate",account_line.fixed_amount*rate)
                         print("vals fixed", vals)
 
-                    if account_line.is_base:
+                        if account_line.is_base:
 
-                        # print("total_taken baseeee>>>",total_taken)
-                        all_remaining = (r.price_unit * r.quantity / rate) - total_taken
+                            # print("total_taken baseeee>>>",total_taken)
+                            all_remaining = (r.price_unit * r.quantity / rate) - total_taken
 
-                        one_remaining = all_remaining * rate / (r.quantity)
+                            one_remaining = all_remaining * rate / (r.quantity)
 
-                        # print("all_remaining",all_remaining)
-                        # print("one_remaining",one_remaining)
 
-                        vals = {
-                            'account_id': account_line.income_account_id.id,
+                            vals = {
+                                'account_id': account_line.income_account_id.id,
 
-                            'debit': all_remaining if r.debit else 0,
-                            'credit': all_remaining if r.credit else 0,
-                            'name': r.name,
-                            'move_id': r.move_id.id,
-                            'price_unit': one_remaining,
-                            'quantity': r.quantity,
-                        }
-                        # print("vals in update",vals)
+                                'debit': all_remaining if r.debit else 0,
+                                'credit': all_remaining if r.credit else 0,
+                                'name': r.name,
+                                'move_id': r.move_id.id,
+                                'price_unit': one_remaining,
+                                'quantity': r.quantity,
+                            }
 
-                        r.with_context(check_move_validity=False).update(vals)
+
+                            r.with_context(check_move_validity=False).update(vals)
 
 
 
-                    else:
-                        # try:
+                        else:
 
-                        r.move_id.with_context(check_move_validity=False)
-                        new_line = r.copy(vals)
-                        new_line.with_context(check_move_validity=False).write(vals)
 
-                    # except:
-                    #     r.move_id.with_context(check_move_validity=False)
-                    #     new_line.with_context(check_move_validity=False).write(vals)
+                            r.move_id.with_context(check_move_validity=False)
+                            new_line = r.copy(vals)
+                            new_line.with_context(check_move_validity=False).write(vals)
+
+
+
+
--- models/cruise.py
+++ models/cruise.py
@@ -8,7 +8,8 @@ class Cruise(models.Model):
     _description = 'Cruise Trip'
     _rec_name='name'
 
-    name = fields.Char(compute='_compute_name',store=1,string='Name',tracking=True)
+    name = fields.Char(compute='_compute_name',store=1,string='Name',tracking=True,inverse="_set_name",copy=False)
+    cruise_name=fields.Selection([("Dahabia",'dahabia'),("Sun",'sun'),("Moon",'moon'),("Star","star"),("Nile",'nile')],default="Dahabia")
     nights=fields.Selection([('3', '3'), ('4', '4'),('7','7')],default='3', string='Nights',tracking=True)
     start_date = fields.Date(string='Start Date',tracking=True,required=True)
 
@@ -29,13 +30,15 @@ class Cruise(models.Model):
 
 
 
-    @api.depends('start_date','company_id')
+    @api.depends('start_date','cruise_name')
     def _compute_name(self):
         for r in self:
-            if r.start_date and r.company_id:
-                r.name= str(r.company_id.name) + ' - ' + r.start_date.strftime('%d-%m-%Y')
+            if r.start_date and r.cruise_name:
+                r.name= str(r.cruise_name) + ' - ' + r.start_date.strftime('%d-%m-%Y')
 
-
+    def _set_name(self):
+        for r in self:
+            pass
     def confirm(self):
         if len(self.cruise_lines):
             self.state="confirmed"
@@ -45,16 +48,18 @@ class Cruise(models.Model):
 
     def create_invoices(self):
         cruise_lines=self.cruise_lines.read_group(domain=[],
-                                                  fields=['id','partner_id','currency_id','guest_nationality','nights','cabinet_number','occupancy','sight_seeing','rate','is_paid_guide'
-                                                                ],groupby=['partner_id','currency_id'],lazy=False)
+                                                  fields=['id','partner_id','currency_id','guest_nationality','nights','cabinet_number','occupancy','sight_seeing','rate','is_paid_guide','cruise_name'
+                                                                ],groupby=['partner_id','currency_id','cruise_name'],lazy=False)
 
         print("cruise_lines",len(cruise_lines))
         for line in cruise_lines:
             print('line..',line)
             currency_id=line['currency_id'][0]
             partner_id=line['partner_id'][0]
+            cruise_name=line['cruise_name']
+            print("cruise_name",cruise_name)
             lines = self.cruise_lines.filtered(
-                lambda l: l.partner_id.id == partner_id and l.currency_id.id == currency_id)
+                lambda l: l.partner_id.id == partner_id and l.currency_id.id == currency_id and l.cruise_name==cruise_name)
             if not len(lines):
                 continue
             invoice_id = self.env['account.move'].create({
@@ -63,12 +68,13 @@ class Cruise(models.Model):
                 'currency_id': currency_id,
                 'cruise_id': self.id,
                 'invoice_date': self.start_date,
+                'cruise_name': cruise_name,
 
             })
             print('invoice_id',invoice_id)
 
 
-            lines=self.cruise_lines.filtered(lambda l :l.partner_id.id==partner_id and l.currency_id.id==currency_id )
+            lines=self.cruise_lines.filtered(lambda l :l.partner_id.id==partner_id and l.currency_id.id==currency_id and l.cruise_name==cruise_name )
             print("lines",lines)
             invoice_lines=[]
             for l in lines:
@@ -78,9 +84,10 @@ class Cruise(models.Model):
                     # print("product_id",product_id)
 
                 else:
+                    print("guid")
                     product_id = self.env['product.product'].search(
-                        [("name","like","%guide%")],limit=1)
-                # print("product_id", product_id.name)
+                        [("name","ilike","%guide%")],limit=1)
+                    print("product_id", product_id.name)
                 # print('persons before',l.cabinet_number*(int(l.occupancy)+0.5*int(l.children)))
                 # # print('l.cabinet_number',l.cabinet_number)
                 # # print('(int(l.occupancy)',int(l.occupancy))
@@ -90,22 +97,25 @@ class Cruise(models.Model):
 
                 invoice_lines.append({
                     'product_id':product_id.id,
-                    'quantity':int(self.nights)*l.cabinet_number,
+                    'quantity':int(l.nights)*l.cabinet_number,
                     'price_unit':l.rate,
                     'move_id':invoice_id.id,
                     'tax_ids':product_id.taxes_id,
                     'sight_seeing':l.sight_seeing,
+                    'sight_seeing_price_person':l.sight_seeing_price_person,
                     'persons':l.cabinet_number*(int(l.occupancy)+0.5*int(l.children)),
                     'main_line':True,
                     'taken_line':False,
                     'default_unit_price':l.rate,
-                    'default_quantity':int(self.nights)*l.cabinet_number,
-                    'default_subtotal':l.rate*int(self.nights)*l.cabinet_number-(l.rate*int(self.nights)*l.cabinet_number*0.12*0.14),
-                    'default_total':l.rate*int(self.nights)*l.cabinet_number,
+                    'default_quantity':int(l.nights)*l.cabinet_number,
+                    'default_subtotal':l.rate*int(l.nights)*l.cabinet_number-(l.rate*int(self.nights)*l.cabinet_number*0.12*0.14),
+                    'default_total':l.rate*int(l.nights)*l.cabinet_number,
                     'nights':l.nights,
                     'children':l.children,
                     'cabinet_number':l.cabinet_number,
                     'analytic_distribution':l.analytic_distribution,
+                    "guest_nationality":l.guest_nationality.id,
+
 
                 })
 
@@ -189,17 +199,18 @@ class CruiseLine(models.Model):
     guest_name=fields.Char(string="Guest Name",tracking=True,)
     guest_nationality=fields.Many2one('res.country',string="Guest Nationality",tracking=True,)
     checkin=fields.Date(string="Checkin date",related='cruise_id.start_date',store=True,tracking=True,)
-    nights=fields.Selection(related='cruise_id.nights',store=1,tracking=True,)
+    nights=fields.Selection([('3', '3'), ('4', '4'),('7','7')],compute='_compute_nights',store=1,tracking=True,inverse="_set_nights",)
     cabinet_number=fields.Integer(default=1)
     cabinet_type=fields.Selection([('CABIN UPGRADE TO SUITE',"cabin upgrade  to suite"),('CABIN',"cabin"),
 
-                                 ("Other","other")],tracking=True,default="cabin",store=True,)
+                                 ("Other","other")],tracking=True,default="CABIN",store=True,)
 
     occupancy=fields.Selection([("1","SGL"),("2","DBL"),("3","TPL")],string="Occupancy",default="2",required=True)
 
     sight_seeing=fields.Boolean(default=False,string="Sight seeing")
     sight_seeing_lang=fields.Char(string="Sight Seeing Language",store=True)
 
+    sight_seeing_price_person=fields.Float(string="Sight Seeing for Person in EGP",help="Sight Seeing for Person in EGP and include taxes(12-14%)")
     currency_id=fields.Many2one('res.currency',string="Currency",tracking=True,required=True)
     rate=fields.Monetary(string="Rate P/Unit/Night",tracking=True,required=True)
     payment_method=fields.Selection([('cash','Cash'),('cheque','Cheque'),("bank transfer","Bank Transfer")])
@@ -211,6 +222,24 @@ class CruiseLine(models.Model):
 
     children=fields.Selection([('0','0'),('1','1'),('2','2')],default='0',string="Children")
     analytic_distribution = fields.Json(compute="_compute_analytic_distribution",store=True,inverse="_compute_dummy")
+    cruise_name = fields.Selection(
+        [("Dahabia", 'dahabia'), ("Sun", 'sun'), ("Moon", 'moon'), ("Star", "star"), ("Nile", 'nile')],
+        default="Dahabia",compute="_compute_cruise_name",store=True,inverse="_set_cruise_name")
+
+    @api.depends('cruise_id.cruise_name')
+    def _compute_cruise_name(self):
+        for record in self:
+            record.cruise_name =record.cruise_id.cruise_name
+    def _set_cruise_name(self):
+       for r in self:
+           pass
+    @api.depends('cruise_id.nights')
+    def _compute_nights(self):
+        for line in self:
+            line.nights=line.cruise_id.nights
+    def _set_nights(self):
+        for line in self:
+            pass
 
     @api.depends('cruise_id.analytic_distribution')
     def _compute_analytic_distribution(self):
